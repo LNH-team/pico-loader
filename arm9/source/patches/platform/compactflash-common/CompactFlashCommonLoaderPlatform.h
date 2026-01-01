b10f83347e35c1518d6adbc2c9bc8fe646577170
Edoardo Lolletti <edoardo762@gmail.com>
2026-01-01 20:53:34 +0100

    Update CompactFlashCommonLoaderPlatform.h

--- arm9/source/patches/platform/compactflash-common/CompactFlashCommonLoaderPlatform.h
+++ arm9/source/patches/platform/compactflash-common/CompactFlashCommonLoaderPlatform.h
@@ -15,12 +15,23 @@ public:
     bool InitializeSdCard() override;
 
 protected:
+    /// @brief Locks/Unlocks the cart to operate on the inserted CF card
+    /// @param Whether the card sould be locked (prevent R/W operations) or unlocked (allows R/W operations)
     virtual void CardLockUnlock(bool lock) const = 0;
 
+    /// @brief Checks if the implementation requires specific lock/unlock commands before using the CF Card
+    ///        If a card requires locking, it should implement \see NewCardLockUnlockPatchCode returning the patch
+    ///        code performing the lock/unlock sequences, and also repeat the same logic in CardLockUnlock.
+    /// @return \c true if the platform requires the sequence, or \c false otherwise.
     virtual bool RequiresLocking() const = 0;
-    
-    virtual CompactFlashLockUnlockPatchCode* NewCardLockUnlockPatchCode(PatchHeap& patchHeap) const    { return nullptr; }
 
+    /// @brief Generates the patch code containing the lock/unlock routines equivalent to \see CardLockUnlock
+    /// @note  The returned routine, is only allowed to modify r0, other registers should be left untouched
+    /// @return A pointer to the allocated \see CompactFlashLockUnlockPatchCode
+    virtual CompactFlashLockUnlockPatchCode* NewCardLockUnlockPatchCode(PatchHeap& patchHeap) const { return nullptr; }
+
+    /// @brief Returns the exposed address associated to the Compact Flash registers
+    /// @return the \see cf_registers_t struct containing the registers
     virtual const cf_registers_t& GetCfRegisters() const = 0;
 
     const SdReadPatchCode* CreateSdReadPatchCode(
@@ -31,14 +42,14 @@ protected:
         {
             return new CompactFlashStatusFunctionsPatchCode(patchHeap, regs);
         });
-        
+
         auto transferSector = patchCodeCollection.GetOrAddSharedPatchCode([&]
         {
             return new CompactFlashTransferSectorPatchCode(patchHeap, regs, statusFunctions);
         });
 
         auto lockUnlock = allocateLockUnlockPatchCode(patchCodeCollection, patchHeap);
-        
+
         return patchCodeCollection.GetOrAddSharedPatchCode([&]
         {
             return new CompactFlashReadSectorPatchCode(patchHeap, regs, transferSector, lockUnlock);
@@ -53,20 +64,20 @@ protected:
         {
             return new CompactFlashStatusFunctionsPatchCode(patchHeap, regs);
         });
-        
+
         auto transferSector = patchCodeCollection.GetOrAddSharedPatchCode([&]
         {
             return new CompactFlashTransferSectorPatchCode(patchHeap, regs, statusFunctions);
         });
-        
+
         auto lockUnlock = allocateLockUnlockPatchCode(patchCodeCollection, patchHeap);
-        
+
         return patchCodeCollection.GetOrAddSharedPatchCode([&]
         {
             return new CompactFlashWriteSectorPatchCode(patchHeap, regs, transferSector, lockUnlock);
         });
     }
-    
+
 private:
 
     const CompactFlashLockUnlockPatchCode* allocateLockUnlockPatchCode(
