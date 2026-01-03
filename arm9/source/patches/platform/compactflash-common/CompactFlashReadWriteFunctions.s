699ae4321e2ba50ab28abdf26234443d0510f4dd
Edoardo Lolletti <edoardo762@gmail.com>
2026-01-03 19:27:38 +0100

    Add comment about "abi"

--- arm9/source/patches/platform/compactflash-common/CompactFlashReadWriteFunctions.s
+++ arm9/source/patches/platform/compactflash-common/CompactFlashReadWriteFunctions.s
@@ -19,6 +19,8 @@
 cf_performTransferSectors:
     push {r0,r1,r4-r7,lr}
     ldr r7, cf_performTransferSectors_waitCardAvailableForCommands
+    @ calls waitCardAvailableForCommands
+    @ this function doesn't alter r0, but sets the zero flag on failure and clears it on error
     bl cf_performTransferSectors_interwork
     beq cf_performTransferSectors_error
 
@@ -66,6 +68,7 @@ cf_performTransferSectors:
     ldr r7, cf_performTransferSectors_waitNextBlockReady
 read_next_block:
     @ calls waitCardNextBlockReady
+    @ this function doesn't alter r0, but sets the zero flag on failure and clears it on error
     bl cf_performTransferSectors_interwork
     beq cf_performTransferSectors_error
 
@@ -139,6 +142,7 @@ readNextSectorBlock:
     ble lastRead
 
     @ calls performTransferSectors
+    @ this function doesn't alter r0, but sets the zero flag on failure and clears it on error
     bl cf_performTransfer_interwork
     beq error
     @ increment sector
@@ -148,6 +152,7 @@ readNextSectorBlock:
 lastRead:
     adds r0, r5
     @ calls performTransferSectors
+    @ this function doesn't alter r0, but sets the zero flag on failure and clears it on error
     bl cf_performTransfer_interwork
 
 error:    
--- arm9/source/patches/platform/compactflash-common/CompactFlashStatusFunctions.s
+++ arm9/source/patches/platform/compactflash-common/CompactFlashStatusFunctions.s
@@ -13,6 +13,7 @@
 .equ CF_CARD_TIMEOUT, 10000000
 
 @ Waits until the card is ready to receive commands
+@ this function doesn't alter r0, but sets the zero flag on failure and clears it on error
 @ bool cf_waitCardAvailableForCommands()
 .global cf_waitCardAvailableForCommands
 .type cf_waitCardAvailableForCommands, %function
@@ -46,6 +47,7 @@ no_longer_busy:
     pop {r0-r4, pc}
 
 @ Waits until the card is ready to write/return the next block
+@ this function doesn't alter r0, but sets the zero flag on failure and clears it on error
 @ bool cf_waitNextBlockReady()
 .global cf_waitNextBlockReady
 .type cf_waitNextBlockReady, %function
