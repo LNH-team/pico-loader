7a3fd6e07a55a1ce75e34efcfd449c57fe0f69f5
R-YaTian <darkbluesky@outlook.com>
2026-01-25 23:55:06 +0800

    TwlAes: Reduce code duplication

--- arm7/source/loader/TwlAes.cpp
+++ arm7/source/loader/TwlAes.cpp
@@ -27,11 +27,7 @@ void TwlAes::SetupAes(const nds_header_twl_t* romHeader) const
     REG_SCFG_EXT |= SCFG_EXT_AES;
     REG_SCFG_CLK |= SCFG_CLK_AES;
 
-    REG_AES_CNT = 0;
-
-    aes_reset();
-    aes_reset();
-    aes_waitKeyBusy();
+    SetupKeySlot(-1, nullptr);
 
     SetupModuleKeyXY(romHeader);
     SetupNandKeyX();
@@ -39,7 +35,7 @@ void TwlAes::SetupAes(const nds_header_twl_t* romHeader) const
     (&REG_AES_SEED0)[KEY_SLOT_NAND * 3].words[3] = NAND_KEYY_WORD_3;
 }
 
-void TwlAes::SetupKeySlot(u32 keySlot, const aes_u128_t* key) const
+void TwlAes::SetupKeySlot(int keySlot, const aes_u128_t* key) const
 {
     REG_AES_CNT = 0;
 
@@ -47,22 +43,21 @@ void TwlAes::SetupKeySlot(u32 keySlot, const aes_u128_t* key) const
     aes_reset();
     aes_waitKeyBusy();
 
-    aes_setKey(keySlot, key);
+    if (key != nullptr && keySlot != -1)
+    {
+        aes_setKey(keySlot, key);
+    }
 
-    aes_setKeySlot(keySlot);
-    aes_waitKeyBusy();
+    if (keySlot != -1)
+    {
+        aes_setKeySlot(keySlot);
+        aes_waitKeyBusy();
+    }
 }
 
 void TwlAes::DecryptModuleAes(void* data, u32 length, const aes_u128_t* iv) const
 {
-    REG_AES_CNT = 0;
-
-    aes_reset();
-    aes_reset();
-    aes_waitKeyBusy();
-
-    aes_setKeySlot(0);
-    aes_waitKeyBusy();
+    SetupKeySlot(KEY_SLOT_MODULE, nullptr);
 
     u32 offset = 0;
     while (length > 0)
--- arm7/source/loader/TwlAes.h
+++ arm7/source/loader/TwlAes.h
@@ -19,7 +19,7 @@ public:
     /// @brief Sets a key into the specified key slot.
     /// @param keySlot The key slot to configure.
     /// @param key The 128-bit AES key to set.
-    void SetupKeySlot(u32 keySlot, const aes_u128_t* key) const;
+    void SetupKeySlot(int keySlot, const aes_u128_t* key) const;
 
 private:
     void SetupModuleKeyXY(const nds_header_twl_t* romHeader) const;
