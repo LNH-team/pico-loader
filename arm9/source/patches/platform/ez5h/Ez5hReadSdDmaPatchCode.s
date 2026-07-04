643a1222ed1ff9a7f4fdf6453cf7ecab65bda3cd
Edoardo Lolletti <edoardo762@gmail.com>
2026-07-04 11:31:50 +0200

    optimize register usage in readdma

--- arm9/source/patches/platform/ez5h/Ez5hReadSdDmaPatchCode.s
+++ arm9/source/patches/platform/ez5h/Ez5hReadSdDmaPatchCode.s
@@ -12,13 +12,13 @@
 @ez5h_readDma(u32 sector, u32 prevSector, u32 channel, void* dst)
 BEGIN_ASM_FUNC ez5h_readDma
 	push {r2-r3,r4-r7,lr}
-	adr r2, read_dma_data
-	@ r2 holds the lower word of EZ5H_CMD_SDMC_READ_DATA
+	adr r3, read_dma_data
+	@ r3 holds the lower word of EZ5H_CMD_SDMC_READ_DATA
 	@ r4 holds miiCardDmaCopy32Ptr
 	@ r5 holds REG_MCCMD0
 	@ r6 holds EZ5H_CTRL_READ_512
 	@ r7 holds sendSDIOCommand
-	ldmia r2, {r2,r4,r5,r6,r7}
+	ldmia r3, {r3,r4,r5,r6,r7}
 
 ez5h_sdhc_readDma_label:
 	lsls r1,r0,#9
@@ -27,36 +27,36 @@ ez5h_sdhc_readDma_label:
 	@ call sendSDIOCommand
 	blx r7
 
+	@ r0 // DMA channel
+	@ r2 // Destination
+	pop {r0,r2}
+
 	@ negative on failure
 	bmi sdio_fail
 
 	@ lower word of EZ5H_CMD_SDMC_READ_DATA
-	movs r3, #0
+	movs r7, #0
 
-	stmia r5!, {r2, r3}
+	stmia r5!, {r3, r7}
 	@ REG_MCCMD0 is incremented by 8 in the stmia, REG_MCCMD0-8 = REG_MCCNT0
 	subs r5, #16
 
-    @ r0 // DMA channel
-    @ r2 // Destination
-	pop {r0,r2}
 	ldr r1, =REG_MCD1
 	@ (1 << 9) = 512 = count
-    movs r3, #1
-    lsls r3, #9
+	movs r3, #1
+	lsls r3, #9
 
 	@ call miiCardDmaCopy32Ptr
-    blx r4
+	blx r4
 
 	@ select rom mode, with irq
-    movs r7, #0xC0 
-    strb r7, [r5,#0x1]
+	movs r7, #0xC0 
+	strb r7, [r5,#0x1]
 	@ write EZ5H_CTRL_READ_512 to mccnt1
-    str r6, [r5,#0x4]
+	str r6, [r5,#0x4]
 
-	pop {r4-r7,pc}
 sdio_fail:
-	pop {r2-r3,r4-r7,pc}
+	pop {r4-r7,pc}
 
 BEGIN_ASM_FUNC ez5h_endDma
 	bx lr
@@ -70,6 +70,6 @@ ez5h_readDma_miiCardDmaCopy32Ptr:
 	.word REG_MCCMD0
 	.word EZ5H_CTRL_READ_512
 ez5h_readDma_sendSDIOCommand:
-    .word 0
+	.word 0
 
 
